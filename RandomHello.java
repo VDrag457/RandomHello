2ac74170fbbbf43e99205c7e45468df8cc89a44c
VDrag457 <126526715+VDrag457@users.noreply.github.com>
2025-08-30 11:28:37 -0400

    Add files via upload

---
+++ RandomHello.java
@@ -0,0 +1,24 @@
+package hw0;
+/**
+* RandomHello selects a random greeting to display to the user.
+*/
+public class RandomHello
+{
+	/**
+	* Uses a RandomHello object to print
+	* a random greeting to the console.
+	*/
+	public static void main(String[] argv)
+	{
+		RandomHello randomHello = new RandomHello();
+		System.out.println(randomHello.getGreeting());
+	}
+
+	/**
+	* @return a random greeting from a list of five different greetings.
+	*/
+	public String getGreeting()
+	{
+	// YOUR CODE GOES HERE
+	}
+}
